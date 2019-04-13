9f39c126e5c0b8834e0dc5c2bead8b6d09200a0d
Fedor <green4er@gmail.com>
2019-04-13 21:07:13 +0300

    :sparkles: API part finished.

--- src/test/java/steps/HttpBinAPIStepDefinitions.java
+++ src/test/java/steps/HttpBinAPIStepDefinitions.java
@@ -1,7 +1,9 @@
 package steps;
 
 import api.HttpBinAPI;
+import api.objects.DelayedResponse;
 import api.objects.ImageExtensions;
+import com.google.common.base.Stopwatch;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.apache.logging.log4j.LogManager;
@@ -11,14 +13,17 @@ import org.junit.Assert;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.concurrent.TimeUnit;
 
 public class HttpBinAPIStepDefinitions {
     private final static Logger logger = LogManager.getLogger();
     private HttpBinAPI api = HttpBinAPI.getInstance();
     private byte[] lastReceivedImage;
+    private DelayedResponse lastReceivedDelayedResponse;
 
     /**
      * Gets image from HttpBin API.
+     *
      * @param imageExtension extension of image.
      */
     @When("^(.+) image is received from API$")
@@ -30,6 +35,7 @@ public class HttpBinAPIStepDefinitions {
 
     /**
      * Compares received image with ideal stored at "test/resources/images".
+     *
      * @param imageExtension extension of image.
      * @throws IOException exception at local image reading.
      */
@@ -44,13 +50,29 @@ public class HttpBinAPIStepDefinitions {
 
     /**
      * Send request and waits for response to be received.
+     *
      * @param responseDelay timeout in seconds.
      */
-    @When("request is sent with {int} seconds response delay parameter")
+    @When("^request is sent with (.+) seconds response delay parameter$")
     public void sendRequestWithResponseDelay(int responseDelay) {
         if (responseDelay > 10) throw new IllegalArgumentException("Parameter can't be more than 10 seconds!");
         logger.info("Getting response with " + responseDelay + " second(s) delay...");
-        Assert.assertNotNull("Can't get response from API!", api.getResponseWithDelay(responseDelay));
+        Stopwatch timer = Stopwatch.createStarted();
+        lastReceivedDelayedResponse = api.getResponseWithDelay(responseDelay);
+        long elapsedTime = timer.stop().elapsed(TimeUnit.SECONDS);
+        Assert.assertFalse("Request execution took less time than was expected!", elapsedTime < responseDelay);
         logger.info("Response was successfully received!");
+    }
+
+    /**
+     * Checks that timeout value is present in URL field of last received DelayedResponse object.
+     *
+     * @param responseDelay timeout in seconds.
+     */
+    @Then("^(.+) seconds delay should be present in response body")
+    public void delayShouldBeInResponse(String responseDelay) {
+        logger.info("Expecting to see \"" + responseDelay + "\" in response's URL field...");
+        Assert.assertTrue("Response does not contains expected value!", lastReceivedDelayedResponse.getUrl().endsWith(responseDelay));
+        logger.info("Expected value was found in response from API!");
     }
 }
--- src/test/resources/features/HttpBin.feature
+++ src/test/resources/features/HttpBin.feature
@@ -1,16 +1,22 @@
 @api @HttpBin
 Feature: HttpBin API
 
-#  Scenario Outline: Get <imageExtension> image from API and compare it with ideal image.
-#    When <imageExtension> image is received from API
-#    Then <imageExtension> image should be equal to ideal one
-#
-#    Examples:
-#      | imageExtension |
-#      | PNG            |
-#      | JPEG           |
-#      | SVG            |
-#      | WEBP           |
+  Scenario Outline: Get <imageExtension> image from API and compare it with ideal image.
+    When <imageExtension> image is received from API
+    Then <imageExtension> image should be equal to ideal one
 
-  Scenario: Wait for response from API.
-    When request is sent with 10 seconds response delay parameter
+    Examples:
+      | imageExtension |
+      | PNG            |
+      | JPEG           |
+      | SVG            |
+      | WEBP           |
+
+  Scenario Outline: Wait <delayTime> seconds for response from API.
+    When request is sent with <delayTime> seconds response delay parameter
+    Then <delayTime> seconds delay should be present in response body
+    Examples:
+      | delayTime |
+      | 1         |
+      | 5         |
+      | 10        |
